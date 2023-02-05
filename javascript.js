c1b4b5f96ff96b6d026a779f7b2592beecdae875
AnsulaDev <ansulaoari88@gmail.com>
2023-02-05 21:57:10 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,23 @@
+
+const section = document.querySelectorAll("section");
+
+window.addEventListener("scroll", ()=>{
+    mainFn();
+});
+mainFn();
+window.addEventListener("resize", () => {
+    window.location.reload();
+});
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+
+
+
+
+
